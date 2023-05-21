7ff12e240a9201cec90d16f95ae937fd700d6ea6
Charifabenfraj <120054243+Charifabenfraj@users.noreply.github.com>
2023-05-21 23:17:20 +0200

    Updating an Entity

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -6,6 +6,7 @@ use App\Entity\VinylMix;
 use App\Repository\VinylMixRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -37,6 +38,23 @@ class MixController extends AbstractController
     {
         return $this->render('mix/show.html.twig', [
             'mix' => $mix,
+        ]);
+    }
+
+    #[Route('/mix/{id}/vote', name: 'app_mix_vote', methods: ['POST'])]
+    public function vote(VinylMix $mix, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $direction = $request->request->get('direction', 'up');
+        if ($direction === 'up') {
+            $mix->setVotes($mix->getVotes() + 1);
+        } else {
+            $mix->setVotes($mix->getVotes() - 1);
+        }
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_mix_show', [
+            'id' => $mix->getId(),
         ]);
     }
 }
